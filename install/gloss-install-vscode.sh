a3a4651899b7c0c0be2bd2c4dc214f17ca215211
George King <george.w.king@gmail.com>
2017-01-30 15:42:30 -0500

    use configurations folder.

--- install/gloss-install-vscode.sh
+++ install/gloss-install-vscode.sh
@@ -4,24 +4,26 @@
 set -eux
 
 VSCODE_GLOSS="$HOME/.vscode-insiders/extensions/gloss"
+VSCODE_CONFIGURATIONS="$VSCODE_GLOSS/configurations"
 VSCODE_SYNTAXES="$VSCODE_GLOSS/syntaxes"
 VSCODE_THEMES="$VSCODE_GLOSS/themes"
 VSCODE_USER="$HOME/Library/Application Support/Code - Insiders/User"
 
-rm -rf "$VSCODE_GLOSS"
+rm -rf "$VSCODE_GLOSS"/*
 
 mkdir -p \
 "$VSCODE_GLOSS" \
+"$VSCODE_CONFIGURATIONS" \
 "$VSCODE_SYNTAXES" \
 "$VSCODE_THEMES" \
 "$VSCODE_USER"
 
 cp \
 package.json \
-swift-gloss.configuration.json \
 "$VSCODE_GLOSS"
 
-cp syntaxes/swift-gloss.json "$VSCODE_SYNTAXES"
-cp _build/gloss-black.json "$VSCODE_THEMES"
+cp configurations/*.json    "$VSCODE_CONFIGURATIONS"
+cp syntaxes/*.json          "$VSCODE_SYNTAXES"
+cp _build/gloss-black.json  "$VSCODE_THEMES"
 
 cp _build/vscode-keys.json "$VSCODE_USER/keybindings.json"
