63ee65f4256b1de8c2cf81a22fa316c9803201bb
George King <george.w.king@gmail.com>
2017-06-28 11:05:57 -0400

    simplify prepend-to-search-paths.py.

--- sh/prepend-to-search-path.py
+++ sh/prepend-to-search-path.py
@@ -15,17 +15,15 @@ or an empty element indicated by '::', as these will be interpreted as '/'.
 from __future__ import print_function
 
 from os import environ
-from sys import argv, stderr
+from sys import argv
 
 
 var_name = argv[1]
 argument_paths = argv[2:]
 
-for p in argument_paths:
+for i, p in enumerate(argument_paths, 2):
   if not p or ':' in p:
-    print('prepend-to-search-path: error: argument is empty or contains colon:', repr(p), file=stderr)
-    print('invocation:', argv, file=stderr)
-    exit(1)
+    exit('prepend-to-search-path: error: argument {} is empty or contains colon: {!r}'.format(i, p))
 
 existing_var = environ.get(var_name)
 # splitting empty string creates a single-element array of the empty string, which is bad.
@@ -33,9 +31,7 @@ existing_paths = existing_var.split(':') if existing_var else []
 
 for p in existing_paths:
   if not p:
-    print('prepend-to-search-path: error: empty component in existing path', file=stderr)
-    print('invocation:', argv, file=stderr)
-    exit(1)
+    exit('prepend-to-search-path: error: empty component in existing path\ninvocation: {}'.format(argv))
 
 remaining_paths = [p for p in existing_paths if p not in argument_paths]
 new_path = ':'.join(argument_paths + remaining_paths)
