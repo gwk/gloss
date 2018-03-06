af194f66d1a6ae4d03edd5d87fcc38f5de91ed41
George King <george.w.king@gmail.com>
2018-03-06 10:08:19 -0500

    json-schema updated to match pithy walk_files changes, including handling of "-" argument.

--- bin/json-schema.py
+++ bin/json-schema.py
@@ -7,20 +7,21 @@ Produce a schema from input json.
 
 import json
 
-from argparse import ArgumentParser
-from sys import stderr, stdout
+from argparse import ArgumentParser, FileType
+from sys import stderr, stdin, stdout
 from pithy.fs import walk_files
 from pithy.schema import compile_schema, write_schema
+
 
 def main():
   parser = ArgumentParser(description='Count lines of source code.')
   parser.add_argument('-count-atoms', action='store_true')
-  parser.add_argument('paths', nargs='+', help='Directories to explore.')
+  parser.add_argument('paths', nargs='*', default=['-'], help='Directories to explore.')
   args = parser.parse_args()
 
   schema = None
-  for path in walk_files(*args.paths, file_exts='.json'):
-    with open(path) as f:
+  for path in walk_files(*args.paths, file_exts=['.json']):
+    with (stdin if path == '-' else open(path)) as f:
       try: j = json.load(f)
       except json.JSONDecodeError as e:
         print('error: {}: {}'.format(path, e), file=stderr)
