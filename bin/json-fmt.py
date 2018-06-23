bacb17586b9335e2e83a249468bc33514192ccba
George King <george.w.king@gmail.com>
2018-06-23 16:30:50 -0400

    move utilities that depend on pithy to pithy repo.

--- bin/json-fmt.py
+++ bin/json-fmt.py
@@ -4,8 +4,6 @@
 from sys import argv, stdin, stdout
 from json import JSONDecoder, dump
 
-from pithy.io import errL
-
 
 if len(argv) > 2:
   exit('requires 1 or no arguments (defaults to std-in)')
@@ -23,4 +21,4 @@ stdout.write('\n')
 
 if end_index < len(s):
   tail = '…' if end_index + 64 < len(s) else ''
-  errL(f'extraneous input beginning at offset {end_index}:\n', s[end_index:end_index + 64].encode('utf8'), tail)
+  exit(f'extraneous input beginning at offset {end_index}:\n', s[end_index:end_index + 64].encode('utf8'), tail)
