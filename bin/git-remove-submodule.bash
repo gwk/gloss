5b79e4c6be737811a36c28b91cd1fdbedbcf64fd
George King <george.w.king@gmail.com>
2018-07-24 17:03:54 -0400

    improve git-remove-submodule.

--- bin/git-remove-submodule.bash
+++ bin/git-remove-submodule.bash
@@ -1,7 +1,12 @@
 # Dedicated to the public domain under CC0: https://creativecommons.org/publicdomain/zero/1.0/.
 
 
-[[ $# == 1 ]] || { echo "$0 requires exactly one argument: the submodule ot remove." 1>&2; exit 1; }
+[[ $# == 1 ]] || { echo "$0 requires exactly one argument: the submodule to remove." 1>&2; exit 1; }
+
+set -e
 
 git submodule deinit "$1"
 git rm "$1"
+echo 'run:'
+echo '$ git commit'
+echo "$ rm -rf .git/modules/$1"
