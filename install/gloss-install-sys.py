b4477a2c07b39efccd5fa80044b2376085128341
George King <george.w.king@gmail.com>
2019-06-15 10:27:06 +0200

    custom symlink for python3 to macOS `Python` executable for better debugging under lldb.

--- install/gloss-install-sys.py
+++ install/gloss-install-sys.py
@@ -5,6 +5,8 @@
 
 from _gloss_install_common import * # parses arguments, etc.
 from pithy.task import * # TODO: remove pithy dependency.
+from os import symlink
+from os.path import exists as path_exists
 
 
 def main():
@@ -16,6 +18,7 @@ def main():
 
     errSL('copying files to dst_dir...')
 
+    # Copy directories.
     sub_dirs = ['sh']
     for d in sub_dirs:
       src_subdir = path_join(src_dir, d)
@@ -51,7 +54,7 @@ def main():
     # install platform-specific bin dir.
     os_bin_dir = path_join(src_dir, 'os', platform, 'bin')
     if is_dir(os_bin_dir):
-      install_bin_dir(os_bin_dir) # platform-specific bin dir
+      install_bin_dir(os_bin_dir)
 
     errSL('generating additional scripts...')
 
@@ -67,8 +70,18 @@ def main():
     else:
       errSL('no platform specifics to gen found at:', bins_os_path)
 
+    # On mac, create a symlink for python3 that points to the MacOS/Python execuctable,
+    # which is appears not to perform the extra exec that python3 does, and is thus more amenable to debugging under lldb.
+    python_exe = '/Library/Frameworks/Python.framework/Versions/3.7/Resources/Python.app/Contents/MacOS/Python'
+    if platform == 'mac' and path_exists(python_exe):
+      python3 = path_join(dst_dir, 'bin', 'python3')
+      errSL(f'adding python3 symlink: {python3} -> {python_exe}')
+      symlink(python_exe, python3)
+
+
   except OSError as e: # usually a permissions problem.
     errSL(e)
     exit(1)
+
 
 if __name__ == '__main__': main()
