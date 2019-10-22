b52857d3106ef17b4ba309388dfc7a2e4299d05a
George King <george.w.king@gmail.com>
2019-10-22 08:42:35 -0400

    dot-view can now take optional input file arguments or default to stdin.

---
+++ bin/dot-view.bash
@@ -0,0 +1,23 @@
+#!/bin/bash
+# Dedicated to the public domain under CC0: https://creativecommons.org/publicdomain/zero/1.0/.
+
+# Split leading positional args from trailing option args in $@.
+inputs=""
+while [[ -n "$@" ]]; do
+  if [[ "$1" == -* ]]; then
+    break
+  else
+    inputs="$args $1"
+    shift
+  fi
+done
+
+if [[ -n "$args" ]]; then
+  inputs=/dev/stdin
+fi
+
+dot $inputs -o /dev/stdout \
+-Tsvg \
+-Grankdir=LR -Gsplines=polyline \
+-Gfontname=sans-serif -Nfontname=sans-serif -Efontname=sans-serif \
+"$@" | html-view
--- bin/dot-view.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-# Dedicated to the public domain under CC0: https://creativecommons.org/publicdomain/zero/1.0/.
-
-dot /dev/stdin -o /dev/stdout \
--Tsvg \
--Grankdir=LR -Gsplines=polyline \
--Gfontname=sans-serif -Nfontname=sans-serif -Efontname=sans-serif \
-"$@" | html-view
