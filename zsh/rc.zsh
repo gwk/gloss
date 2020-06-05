b851e179d8ea80768b060c06e9bbf860e733271d
George King <george.w.king@gmail.com>
2020-06-05 14:50:47 -0700

    Improve rc.zsh.

--- zsh/rc.zsh
+++ zsh/rc.zsh
@@ -18,10 +18,6 @@ export NODE_REPL_MODE=strict
 
 # Command prompt customization.
 
-# Save the starting shell level.
-[[ -z $GLOSS_SHLVL_INITIAL ]] && export GLOSS_SHLVL_INITIAL=$SHLVL
-export GLOSS_SHLVL_TEST="$(( $GLOSS_SHLVL_INITIAL + 1 ))L"
-
 # The user can override these at any time.
 [[ -z $GLOSS_PS_SYMBOL ]] && export GLOSS_PS_SYMBOL='$'
 [[ -z $GLOSS_SSH_SYMBOL ]] && export GLOSS_SSH_SYMBOL='§'
@@ -30,6 +26,22 @@ export GLOSS_SHLVL_TEST="$(( $GLOSS_SHLVL_INITIAL + 1 ))L"
 [[ -z $GLOSS_PS_VENV_STYLE ]] && export GLOSS_PS_VENV_STYLE=$BOLD$TXT_C
 [[ -z $GLOSS_PS_GIT_STYLE ]] && export GLOSS_PS_GIT_STYLE=$BOLD$TXT_M
 
+# Save the starting shell level.
+[[ -z $GLOSS_SHLVL ]] && export GLOSS_SHLVL=$SHLVL
+
+# shell level prefix.
+export GLOSS_PS_PREFIX_LVL=''
+if [[ $SHLVL -gt $GLOSS_SHLVL ]]; then
+  export GLOSS_PS_PREFIX_LVL="$(( SHLVL - GLOSS_SHLVL )) "
+fi
+
+# Prompt sudo prefix.
+export GLOSS_PS_PREFIX_SUDO=''
+if [[ -n "$SUDO_USER" ]]; then
+  export GLOSS_PS_PREFIX_SUDO="$SUDO_USER "
+fi
+
+# Prompt user color.
 if [[ $(whoami) == root ]]; then
   export GLOSS_PS_USER_STYLE=$TXT_R
 else
@@ -39,39 +51,48 @@ fi
 update_terminal_prompt() {
   local exit_status=$?
 
+  # Green prompt if last command exited cleanly (0); otherwise red.
+  if [[ $_exit_status -eq 0 ]]; then
+    local prompt_color=$TXT_G
+  else
+    local prompt_color=$TXT_R
+  fi
+
   local ssh=''
   [[ -n "$SSH_TTY" ]] && local ssh="$GLOSS_SSH_SYMBOL "
 
   local venv=''
   [[ -n $VIRTUAL_ENV ]] && local venv=" $(basename $(dirname $VIRTUAL_ENV))"
+  local _git=$(git-prompt 2>/dev/null)
+  local _git_escaped=${_git/\%/%%} # Double any percent symbols, as that is the escape character for prompt expansion.
+
 
   # %(<N>L..) is the ternary test against the current SHLVL.
   # %(!..) is the ternary test for escalated privileges.
-  # %n $USERNAME.
-  # %~ The current working directory, with $HOME abbreviation.
+  # %n is $USERNAME.
+  # %~ is the current working directory, with $HOME abbreviation.
   # %(?..) is the ternary test for last exit code.
-  local _git=$(git-prompt 2>/dev/null)
-  local _git_escaped=${_git/\%/%%} # Double any percent symbols, as that is the escape character for prompt expansion.
-  
   PS1="\
+%{\
 $RST\
 $GLOSS_PS_PREFIX_STYLE\
 $ssh\
-%($GLOSS_SHLVL_TEST.$SHLVL .)\
-%(!.$SUDO_USER .)\
-$GLOSS_PS_USER_STYLE%n \
-$GLOSS_PS_PATH_STYLE%~\
+$GLOSS_PS_PREFIX_LVL\
+$GLOSS_PS_PREFIX_SUDO\
+$GLOSS_PS_USER_STYLE$USERNAME \
+$GLOSS_PS_PATH_STYLE\
+%}\
+%~\
+%{\
 $GLOSS_PS_VENV_STYLE$venv\
 $GLOSS_PS_GIT_STYLE$_git_escaped\
 $RST \
-%(?.$TXT_G.$TXT_R)$GLOSS_PS_SYMBOL \
-$RST"
-  
-  # bash continuation prompt
-  PS2="\
+$prompt_color$GLOSS_PS_SYMBOL \
 $RST\
-$TXT_Y$GLOSS_PS_SYMBOL \
-$RST"
+%}"
+
+  # Continuation prompt.
+  #PS2="%{$RST$TXT_Y$GLOSS_PS_SYMBOL$RST%}"
 }
 
 precmd_functions=(
