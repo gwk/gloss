cae84ff27be8ea5fbcb317863a80e218adf12dd6
George King <george.w.king@gmail.com>
2020-07-16 10:00:21 -0400

    Split out vscode/copy-user-settings.sh.

--- install/gloss-install-vscode.sh
+++ install/gloss-install-vscode.sh
@@ -16,13 +16,3 @@ cp _build/vscode/gloss-black.json  "$VSCODE_THEMES"
 VSCODE_USER="$HOME/Library/Application Support/Code - Insiders/User"
 mkdir -p "$VSCODE_USER"
 cp _build/vscode/keybindings.json  "$VSCODE_USER/keybindings.json"
-set +x
-touch "$VSCODE_USER/settings.json"
-if ! d vscode/settings.json "$VSCODE_USER/settings.json"; then
-  confirm 'format user settings and copy to this repo' \
-  && json-fmt-in-place "$VSCODE_USER/settings.json" \
-  && cp "$VSCODE_USER/settings.json"  vscode/settings.json \
-  || echo "NOTE: user settings are not in sync with repo.
-To format your settings: json-fmt-in-place vscode/settings.json
-To overwrite your settings: cp vscode/settings.json '$VSCODE_USER/settings.json'"
-fi
---
+++ vscode/copy-user-settings.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+# Dedicated to the public domain under CC0: https://creativecommons.org/publicdomain/zero/1.0/.
+
+set -eux
+
+VSCODE_USER="$HOME/Library/Application Support/Code - Insiders/User"
+
+mkdir -p "$VSCODE_USER"
+
+# Diff the user settings and ask user if we should copy them into this repo.
+set +x
+touch "$VSCODE_USER/settings.json"
+if ! d vscode/settings.json "$VSCODE_USER/settings.json"; then
+  confirm 'format user settings and copy to this repo' \
+  && json-fmt-in-place "$VSCODE_USER/settings.json" \
+  && cp "$VSCODE_USER/settings.json"  vscode/settings.json \
+  || echo "NOTE: user settings are not in sync with repo.
+To format your settings: json-fmt-in-place vscode/settings.json
+To overwrite your settings: cp vscode/settings.json '$VSCODE_USER/settings.json'"
+fi
