a795a0b4daddae9deccd1ee77462f6d1111a09fe
George King <george.w.king@gmail.com>
2021-08-25 21:16:25 -0400

    Update `code` bin to use non-insiders path.

--- bin/code.sh
+++ bin/code.sh
@@ -1,2 +1,2 @@
-Contents='/Applications/Visual Studio Code - Insiders.app/Contents'
+Contents='/Applications/Visual Studio Code.app/Contents'
 ELECTRON_RUN_AS_NODE=1 exec "$Contents/MacOS/Electron" "$Contents/Resources/app/out/cli.js" "$@"
