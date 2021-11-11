674a6a654a9678e8c42bfb9c1c55547254d1f250
George King <george.w.king@gmail.com>
2021-11-11 09:38:10 -0500

    Update bin/code.sh to match latest VS Code.

--- bin/code.sh
+++ bin/code.sh
@@ -1,2 +1,2 @@
 Contents='/Applications/Visual Studio Code.app/Contents'
-ELECTRON_RUN_AS_NODE=1 exec "$Contents/MacOS/Electron" "$Contents/Resources/app/out/cli.js" "$@"
+ELECTRON_RUN_AS_NODE=1 exec "$Contents/MacOS/Electron" --ms-enable-electron-run-as-node "$Contents/Resources/app/out/cli.js" "$@"
