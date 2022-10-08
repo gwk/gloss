ce2b4feacb6d795ab15bdafd25320b9405163e56
George King <george@theorypractice.dev>
2022-10-08 15:09:33 -0400

    text-cat-repr, text-cat: type hints and improved error message.

--- bin/text-cat-repr.py
+++ bin/text-cat-repr.py
@@ -1,18 +1,20 @@
 #!/usr/bin/env python3
 # Dedicated to the public domain under CC0: https://creativecommons.org/publicdomain/zero/1.0/.
 
-# cat text files, which will be decoded as utf8 one line at a time.
-# files that contain invalid utf8 will cause exceptions.
+# Print text file line reprs, which will be decoded as utf8 one line at a time.
+# Lines that contain invalid utf8 will print '!INVALID-UTF8:', the line number, and the repr of the bytes.
 
 import sys
+from typing import BinaryIO
 
-def cat_file(bf):
-  for i, line in enumerate(bf):
+
+def cat_file(bf:BinaryIO) -> None:
+  for i, line in enumerate(bf, 1):
     try:
       text = line.decode('utf8')
       print(repr(text))
     except UnicodeDecodeError as e:
-      print('!!INVALID-UTF8: ' + repr(line))
+      print(f'!INVALID-UTF8:{i}:', repr(line))
 
 
 paths = sys.argv[1:]
