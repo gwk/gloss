fa15d9a0c511a287151ef4dd5d8e420e6a225a68
George King <george@theorypractice.dev>
2022-10-14 10:00:46 -0400

    Alias sqlite.

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -3,6 +3,8 @@ alias cp='cp -i' # confirm overwrites interactively.
 alias mv='mv -i' # confirm overwrites interactively.
 alias rm='rm -i' # confirm deletes interactively; encourages use of `del`.
 
+alias sqlite='sqlite3'
+
 alias venv-create-3.9='python3.9 -m venv ./venv3.9'
 alias venv-activate-3.9='source ./venv3.9/bin/activate'
 
