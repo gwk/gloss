35db7f93eeffd0188ece4657eb04b65bba7e927f
George King <george@theorypractice.dev>
2022-11-07 13:59:59 -0500

    sqlite-wal, pym commands.

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -3,6 +3,8 @@ alias cp='cp -i' # confirm overwrites interactively.
 alias mv='mv -i' # confirm overwrites interactively.
 alias rm='rm -i' # confirm deletes interactively; encourages use of `del`.
 
+alias pym='python -m' # run python module.
+
 alias sqlite='sqlite3'
 
 alias venv-create-3.9='python3.9 -m venv ./venv3.9'
