c5bef45c1f2bb45c21cd5c49d82ad8aa8c8edfa9
George King <george@theorypractice.dev>
2024-11-01 15:12:14 -0700

    aliases.zsh: remove outdated aliases.

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -6,8 +6,3 @@ alias rm='rm -i' # confirm deletes interactively; encourages use of `del`.
 alias pym='python -m' # run python module.
 
 alias sqlite='sqlite3'
-
-alias venv-create-3.9='python3.9 -m venv ./venv3.9'
-alias venv-activate-3.9='source ./venv3.9/bin/activate'
-
-alias help='run-help'
