84e02d11f95e43dc8bc8032bb6588f0c8257a228
George King <george@theorypractice.dev>
2024-11-12 10:10:54 -0800

    pythonstartup.py: sort imports.

--- pythonstartup.py
+++ pythonstartup.py
@@ -1,55 +1,100 @@
 
 # Import typing first so that other symbols take precedence.
 # This is especially desirable for the collections symbols; the typing versions are deprecated.
-import typing; from typing import *
+import collections
+import collections.abc
+import dataclasses
+import enum
+import functools
+import importlib
+import inspect
+import io
+import itertools
+import math
+import operator
+import os
+import re
+import sys
+import time
+import typing
+import zoneinfo
+from collections import *
+from collections.abc import *
+from dataclasses import *
+from enum import *
+from functools import *
+from importlib import *
+from inspect import *
+from io import *
+from itertools import *
+from math import *
+from operator import *
+from os import environ
+from sys import argv, stderr, stdout
+from time import *
+from typing import *
+from zoneinfo import *
 
-import collections; from collections import *
-import collections.abc; from collections.abc import *
-import dataclasses; from dataclasses import *
-import enum; from enum import *
-import functools; from functools import *
-import importlib; from importlib import *
-import inspect; from inspect import *
-import io; from io import *
-import itertools; from itertools import *
-import math; from math import *
-import operator; from operator import *
-import os; from os import environ
-import re;
-import sys; from sys import argv, stderr, stdout
-import time; from time import *
-import zoneinfo; from zoneinfo import *
-
-import pithy.ansi; from pithy.ansi import *
-import pithy.ast; from pithy.ast import *
-import pithy.buffer; from pithy.buffer import *
-import pithy.collection; from pithy.collection import *
-import pithy.clock; from pithy.clock import *
-import pithy.csv; from pithy.csv import *
-import pithy.date; from pithy.date import *
-import pithy.default; from pithy.default import *
-import pithy.desc; from pithy.desc import *
-import pithy.dict; from pithy.dict import *
-import pithy.format; from pithy.format import *
-import pithy.fs; from pithy.fs import *
-import pithy.graph; from pithy.graph import *
-import pithy.io; from pithy.io import *
-import pithy.iterable; from pithy.iterable import *
-import pithy.json; from pithy.json import *
-import pithy.loader; from pithy.loader import *
-import pithy.path; from pithy.path import *
-import pithy.path_encode; from pithy.path_encode import *
-import pithy.range; from pithy.range import *
-import pithy.reprs; from pithy.reprs import *
-import pithy.schema; from pithy.schema import *
-import pithy.sequence; from pithy.sequence import *
-import pithy.string; from pithy.string import *
-import pithy.svg; from pithy.svg import *
-import pithy.task; from pithy.task import *
-import pithy.types; from pithy.types import *
-import pithy.untyped; from pithy.untyped import *
-import pithy.url; from pithy.url import *
-import pithy.util; from pithy.util import *
+import pithy.ansi
+import pithy.ast
+import pithy.buffer
+import pithy.clock
+import pithy.collection
+import pithy.csv
+import pithy.date
+import pithy.default
+import pithy.desc
+import pithy.dict
+import pithy.format
+import pithy.fs
+import pithy.graph
+import pithy.io
+import pithy.iterable
+import pithy.json
+import pithy.loader
+import pithy.path
+import pithy.path_encode
+import pithy.range
+import pithy.reprs
+import pithy.schema
+import pithy.sequence
+import pithy.string
+import pithy.svg
+import pithy.task
+import pithy.types
+import pithy.untyped
+import pithy.url
+import pithy.util
+from pithy.ansi import *
+from pithy.ast import *
+from pithy.buffer import *
+from pithy.clock import *
+from pithy.collection import *
+from pithy.csv import *
+from pithy.date import *
+from pithy.default import *
+from pithy.desc import *
+from pithy.dict import *
+from pithy.format import *
+from pithy.fs import *
+from pithy.graph import *
+from pithy.io import *
+from pithy.iterable import *
+from pithy.json import *
+from pithy.loader import *
+from pithy.path import *
+from pithy.path_encode import *
+from pithy.range import *
+from pithy.reprs import *
+from pithy.schema import *
+from pithy.sequence import *
+from pithy.string import *
+from pithy.svg import *
+from pithy.task import *
+from pithy.types import *
+from pithy.untyped import *
+from pithy.url import *
+from pithy.util import *
 
 
 try: import jedi.utils
