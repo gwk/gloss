2bda723520fde0c7ef788f6e7e6f2a1bb652add3
George King <george@theorypractice.dev>
2025-05-07 09:29:33 -0700

    bin/gchbpu.bash: default to "origin" branch.

--- bin/gchbpu.bash
+++ bin/gchbpu.bash
@@ -1,7 +1,26 @@
 #!/usr/bin/env bash
 # Dedicated to the public domain under CC0: https://creativecommons.org/publicdomain/zero/1.0/.
 
-[[ $# == 2 ]] || { echo "requires exactly two arguments (remote new-branch-name)." 1>&2; exit 1; };
+set -e
 
-git checkout -b "$2" && git push -u "$1" "$2"
+if [[ $# == 1 ]]; then
+  remote="origin"
+elif [[ $# == 2 ]]; then
+  remote="$1"
+  shift
+else
+  echo "requires one or two arguments (remote new-branch-name)." 1>&2
+  exit 1
+fi
+branch="$1"
 
+# Check if the branch already exists.
+if git show-ref --verify --quiet refs/heads/"$branch"; then
+  echo "Branch '$branch' already exists." 1>&2
+  git checkout "$branch"
+else
+  # Create the new branch.
+  git checkout -b "$branch"
+fi
+
+git push -u "$remote" "$branch"
