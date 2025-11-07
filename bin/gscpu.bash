89bfe14d105e09eb3a84aa3224143fdc7d8c8c7d
George King <george@theorypractice.dev>
2025-11-07 13:56:40 -0800

    Rename gchbpu -> gscpu; update to use switch.

--- bin/gscpu.bash
+++ bin/gscpu.bash
@@ -3,13 +3,15 @@
 
 set -e
 
+# Git switch create push upstream.
+
 if [[ $# == 1 ]]; then
   remote="origin"
 elif [[ $# == 2 ]]; then
   remote="$1"
   shift
 else
-  echo "requires one or two arguments (remote new-branch-name)." 1>&2
+  echo "gscpu requires one or two arguments (remote new-branch-name)." 1>&2
   exit 1
 fi
 branch="$1"
@@ -18,11 +20,11 @@ branch="$1"
 if git show-ref --verify --quiet refs/heads/"$branch"; then
   echo "Branch '$branch' already exists." 1>&2
   set -x
-  git checkout "$branch"
+  git switch "$branch"
 else
   # Create the new branch.
   set -x
-  git checkout -b "$branch"
+  git switch -c "$branch"
 fi
 
 git push -u "$remote" "$branch"
