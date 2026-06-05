fd2ec24ad6353b1bbb0606d2282ad3a9755788a4
George King <george@theorypractice.dev>
2026-06-04 19:22:26 -0700

    aliases.zsh: guard interactive commands.

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,8 +1,11 @@
 
-alias cp='cp -i' # confirm overwrites interactively.
-alias mv='mv -i' # confirm overwrites interactively.
-alias rm='rm -i' # confirm deletes interactively; encourages use of `del`.
-
 alias pym='python -m' # run python module.
 
 alias sqlite='sqlite3'
+
+
+if [[ -o interactive ]]; then # Guard this to prevent Claude Code from getting tripped up with its weird shell setup.
+  alias cp='cp -i' # confirm overwrites interactively.
+  alias mv='mv -i' # confirm overwrites interactively.
+  alias rm='rm -i' # confirm deletes interactively; encourages use of `del`.
+fi
