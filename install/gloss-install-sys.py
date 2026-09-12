c844d359579e13599cc11c95a5ad44a590eb0e14
George King <george@theorypractice.dev>
2026-09-12 09:50:47 -0700

    Install dotfiles in /opt/gloss/dotfiles for reference.

--- install/gloss-install-sys.py
+++ install/gloss-install-sys.py
@@ -88,7 +88,7 @@ def main() -> None:
     errSL('copying files to dst_dir…')
 
     # Copy directories.
-    sub_dirs = ['zsh']
+    sub_dirs = ['zsh', 'dotfiles']
     for d in sub_dirs:
       src_subdir = path_join(src_dir, d)
       dst_subdir = path_join(dst_dir, d)
