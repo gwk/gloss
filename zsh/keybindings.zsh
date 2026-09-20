c4518d57be974ef2953784745be76ea6965b00d2
George King <george@theorypractice.dev>
2026-09-20 15:44:53 -0700

    zsh/: Fix stty compatibility on Linux.

--- zsh/keybindings.zsh
+++ zsh/keybindings.zsh
@@ -70,11 +70,13 @@ if (( $+commands[fzf] )); then
 fi
 
 stty discard undef # Disable tty output discard; Ctrl-O.
-stty dsusp undef # Disable delayed suspend; Ctrl-Y.
+if [[ $OSTYPE == darwin* ]]; then
+  stty dsusp undef # Disable delayed suspend; Ctrl-Y.
+fi
 stty kill undef # Disable tty kill-line; Ctrl-U.
 stty lnext undef # Disable tty literal-next character; Ctrl-V.
 stty quit undef # Disable SIGQUIT character; Ctrl-\.
-stty reprint undef # Disable tty line reprint; Ctrl-R.
+stty rprnt undef # Disable tty line reprint; Ctrl-R.
 stty werase undef # Disable tty erase-word; Ctrl-W.
 
 unsetopt FLOW_CONTROL # Disable software flow control (stop/XOFF, start/XON): frees Ctrl-S, Ctrl-Q.
